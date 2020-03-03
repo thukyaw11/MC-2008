cab5db7c4f3ccdad14f57393d669db59879cc9ef
thukyaw11 <helloeleven968@gmail.com>
2020-03-03 22:50:34 +0630

    mark down

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import "./registerServiceWorker";
 import router from "./router";
 import Button from 'ant-design-vue/lib/button'; // for js
 import 'ant-design-vue/dist/antd.css';
+import VueMarkDown from "vue-markdown";
+
+
 Vue.config.productionTip = false;
 Vue.use(Button);
+Vue.use(VueMarkDown);
 new Vue({
   router,
   render: h => h(App)
--- src/views/SingleView.vue
+++ src/views/SingleView.vue
@@ -1,42 +1,46 @@
 <template>
-  <div class="singleView">
-    <NavBar :title="singleView.title"/>
-    <div id="content">
-      <p>{{singleView.content}}</p>
-    </div>
-  </div>
+<div class="singleView">
+    <NavBar :title="singleView.title" />
+    <p id="content">
+      <VueMarkDown html>{{singleView.content}}</VueMarkDown>
+    </p>
+</div>
 </template>
 
 <style>
-  #content{
+#content {
     margin-top: 70px;
     padding: 10px;
-  }  
+}
 </style>
 
 <script>
 /* eslint-disable no-console */
 import NavBar from "../components/NavBar";
-import { Data } from "../content/data.js";
+import VueMarkDown from 'vue-markdown'; 
+import {
+    Data
+} from "../content/data.js";
+
 
 export default {
-  components: {
-    NavBar
-  },
-  data() {
-    return {
-      data: "",
-      singleView: []
-    };
-  },
-  props: ["id"],
-  mounted() {
-    this.data = Data;
-    this.singleView = this.data[this.$props.id];
+    components: {
+        NavBar,
+        VueMarkDown
+    },
+    data() {
+        return {
+            data: "",
+            singleView: []
+        };
+    },
+    props: ["id"],
+    mounted() {
+        this.data = Data;
+        this.singleView = this.data[this.$props.id];
 
-  }
+    }
 };
 
 /* eslint-enable no-console */
 </script>
-
