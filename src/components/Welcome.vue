c75ffe41406eecc178a86fd28beb7c66b9628647
thukyaw11 <helloeleven968@gmail.com>
2020-03-10 23:39:32 +0630

    social

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="col-12 text-center pt-4">
-          <h5>ရပ်ကွက် သို့မဟုတ် ကျေးရွာအုပ်စု အုပ်ချုပ်ရေးဥပဒေ</h5>
-      <router-link to="/home">    
-           <md-button class="md-raised md-accent" id="startBtn">စဖတ်ရန်</md-button>
-      </router-link>
-      <br>
+    <h5>ရပ်ကွက် သို့မဟုတ် ကျေးရွာအုပ်စု အုပ်ချုပ်ရေးဥပဒေ</h5>
+    <router-link to="/home">
+      <md-button class="md-raised md-accent" id="startBtn">စဖတ်ရန်</md-button>
+    </router-link>
+    <br />
 
-      <img src="../assets/flag.jpg" alt="open myanmar initiative" id="flag-img">
+    <img src="../assets/flag.jpg" alt="open myanmar initiative" id="flag-img" />
+    <br />
+    <div id="icon_area">
+      <a href="https://www.facebook.com/OpenMyanmarInitiative/" target="_blank">
+        <a-icon type="facebook" theme="filled" :style="{ fontSize: '30px', color: 'grey' }" />
+      </a>
+      <a href="mailto:kyaw261517@gmail.com" target="_blank">
+        <a-icon type="mail" theme="filled" :style="{ fontSize: '30px', color: 'grey' }" />
+      </a>
+      <a href="tel:09254272220" target="_blank">
+        <a-icon type="phone" theme="filled" :style="{ fontSize: '30px', color: 'grey' }" />
+      </a>
+      <a href="https://www.google.com/maps/place/Open+Myanmar+Initiative/@16.8493086,96.1756018,17z/data=!3m1!4b1!4m5!3m4!1s0x30c193460862b387:0x98733ef84d9a1817!8m2!3d16.8493086!4d96.1777905" target="_blank">
+        <a-icon type="environment" theme="filled" :style="{ fontSize: '30px', color: 'grey' }" />
+      </a>
+    </div>
   </div>
 </template>
-
 <script>
 /* eslint-disable no-console */
 
 export default {
-  name : 'Welcome',
-}
+  name: "Welcome"
+};
 
 /* eslint-enable no-console */
 </script>
+
+<style>
+#icon_area {
+  margin-top: 70px;
+  display: flex-box;
+}
+#icon_area a {
+  padding: 10px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import Button from 'ant-design-vue/lib/button'; // for js
+import Icon from 'ant-design-vue/lib/icon';
 import 'ant-design-vue/dist/antd.css';
 import VueMarkDown from "vue-markdown";
 import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
@@ -13,9 +14,10 @@ import MaterialDesignTransition from 'vue-router-md-transition';
 Vue.config.productionTip = false;
 Vue.use(MaterialDesignTransition);
 Vue.use(Button);
-Vue.use(MdButton)
-Vue.use(MdContent)
-Vue.use(MdTabs)
+Vue.use(Icon);
+Vue.use(MdButton);
+Vue.use(MdContent);
+Vue.use(MdTabs);
 Vue.use(VueMarkDown);
 new Vue({
   router,
